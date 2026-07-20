44258f10703a54b75e041905d1db5422908a9996
Irfan <irfanalamnps@gmail.com>
2026-07-20 16:57:23 +0530

    Add StoreVariables.java to demonstrate variable usage

---
+++ Java-Basics/StoreVariables.java
@@ -0,0 +1,11 @@
+class learn
+{
+    public static void main(String a[])
+    {
+        System.out.println(3+5);
+        int num1 = 8;
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
