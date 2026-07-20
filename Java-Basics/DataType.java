c1b2d1c3ed399cebafb993854402277a645aaa0d
Irfan <irfanalamnps@gmail.com>
2026-07-20 17:44:34 +0530

    Add DataType class with variable declarations

---
+++ Java-Basics/DataType.java
@@ -0,0 +1,17 @@
+class learn
+{
+    public static void main(String a[])
+    {
+        int num1 = 9;
+        byte by = 46;
+        short sh = 930;
+        long l = 304l;
+
+        float f = 2.45f;
+        double d = 45.6;
+
+        char c = 403;
+
+        boolean b = true;
+    }
+}
