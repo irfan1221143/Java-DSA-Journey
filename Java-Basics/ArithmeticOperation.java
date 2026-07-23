6a23fa46a5508ad338b0f6dfa41cb6811b0f6a43
Irfan <irfanalamnps@gmail.com>
2026-07-23 18:56:32 +0530

    Add ArithmeticOperation class with basic operations
    Implement basic arithmetic operations including addition, subtraction, multiplication, and division.

---
+++ Java-Basics/ArithmeticOperation.java
@@ -0,0 +1,23 @@
+class learn
+{
+    public static void main(String a[])
+    {
+        int num1 = 7;
+        int num2 = 5;
+        int add = num1 + num2;
+        int sub = num1 - num2;
+        int mult = num1 * num2;
+        int div = num1/num2;  //Division without Decimal
+        double num3 = 7;
+        double num4 = 5;
+        double div1 = num3/num4;  //Division with Decimal
+        int div2 = num1%num2;  //Remainder after division
+        
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(mult);
+        System.out.println(div);
+        System.out.println(div1);
+        System.out.println(div2);
+    }
+}
