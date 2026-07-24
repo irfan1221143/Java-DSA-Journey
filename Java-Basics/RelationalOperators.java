95131b449c74fc4fd542893c58c3ee7c751f40b5
Irfan <irfanalamnps@gmail.com>
2026-07-24 20:58:10 +0530

    Add RelationalOperators.java to demonstrate comparisons
    Implement a Java program demonstrating relational operators.

---
+++ Java-Basics/RelationalOperators.java
@@ -0,0 +1,20 @@
+class learn
+{
+    public static void main(String a[])
+    {
+        int num1 = 7;
+        int num2 = 5;
+
+        boolean greater = num1 > num2;
+        boolean lesser = num1 < num2;
+        boolean greater2 = num1 >= num2;
+        boolean lesser2 = num1 <= num2;
+        boolean equal = num1 == num2;
+
+        System.out.println(greater);
+        System.out.println(lesser);
+        System.out.println(greater2);
+        System.out.println(lesser2);
+        System.out.println(equal);
+    }
+}
