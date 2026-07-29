72a332c3c5b966f4b8397d7e594d3968526f73be
Irfan <irfanalamnps@gmail.com>
2026-07-29 22:37:02 +0530

    Update 08_WhileLoop.java

--- Java-Basics/08_WhileLoop.java
+++ Java-Basics/08_WhileLoop.java
@@ -1,3 +1,4 @@
+//While Loop 1
 class learn
 {
     public static void main(String a[])
@@ -8,6 +9,26 @@ class learn
         {
             System.out.println("Hello " + i);
             
+            i++;
+        }
+    }
+}
+
+//While Loop 2
+class learn
+{
+    public static void main(String a[])
+    {
+        int i = 1;
+        while(i<=5)
+        {
+            System.out.println("Hello " + i);
+            int j = 1;
+            while (j<=3)
+            {
+                System.out.println("Hi " + j);
+                j++;
+            }
             i++;
         }
     }
