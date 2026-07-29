333b8ca0281882208e9206e718b3c434246b492c
Irfan <irfanalamnps@gmail.com>
2026-07-29 17:06:31 +0530

    Add TernaryOperator class to demonstrate ternary operator

---
+++ Java-Basics/TernaryOperator.java
@@ -0,0 +1,10 @@
+class learn
+{
+    public static void main(String a[])
+    {
+        int n = 5;
+        int result = 0;
+        result = n%2 == 0 ? 10 /* For True Statement */ : 20 /* For False Statement */;
+        System.out.println(result);
+    }
+}
