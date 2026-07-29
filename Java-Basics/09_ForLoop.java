d10d311052f64a771227179fdc2bbcccbd1b9c23
Irfan <irfanalamnps@gmail.com>
2026-07-29 23:10:36 +0530

    Add nested for loops to print greetings

---
+++ Java-Basics/09_ForLoop.java
@@ -0,0 +1,16 @@
+class learn
+{
+    public static void main(String a[])
+    {
+        for(int i=1;i<=5;i++)
+        {
+            System.out.println("Hello " + i);
+            
+            for(int j=1;j<=3;j++)
+            {
+                System.out.println("Hi " + j);
+            }
+        }
+    System.out.println("Bye");
+    }
+}
