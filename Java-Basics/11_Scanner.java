5f76bf5b65275ebf42f2211702ece9f6a135f5c2
Irfan <irfanalamnps@gmail.com>
2026-07-30 16:50:42 +0530

    Add Scanner example for user input in Java

---
+++ Java-Basics/11_Scanner.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class learn
+{
+    public static void main(String a[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter: ");
+        int num = sc.nextInt();
+
+        System.out.println(num + " is Entered");
+    }
+}
