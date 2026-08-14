a964da2dd6ca50e60277c381428b29ce93e5c5f9
Irfan <irfanalamnps@gmail.com>
2026-08-14 18:35:21 +0530

    Add Calculator class with add and sub methods

---
+++ Java-Basics/12_MethodOverloading.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class Calculator
+{
+    public int add(int n1, int n2)
+    {
+        return n1 + n2;
+    }
+    public int sub(int n1, int n2)
+    {
+        return n1 - n2;
+    }
+}
+
+public class learn
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = sc.nextInt();
+        System.out.print("Enter b: ");
+        int b = sc.nextInt();
+
+        Calculator calc = new Calculator();
+        int r1 = calc.add(a,b);
+        System.out.println("Sum of numbers is " + r1);
+
+        Calculator calc2 = new Calculator();
+        int r2 = calc.sub(a,b);
+        System.out.println("Subtraction of numbers is " + r2);
+    }
+}
