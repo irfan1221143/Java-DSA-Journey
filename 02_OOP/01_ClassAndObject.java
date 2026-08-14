5560b1573cbfebc71cd86bd6f5c2c1353bd879a6
Irfan <irfanalamnps@gmail.com>
2026-08-14 19:00:32 +0530

    Create 01_ClassAndObject.java

---
+++ 02_OOP/01_ClassAndObject.java
@@ -0,0 +1,21 @@
+class Student
+{
+    String name;
+    int age;
+}
+
+public class Learn
+{
+    public static void main(String args[])
+    {
+        Student s1 = new Student();
+
+        s1.name = "Irfan";
+        s1.age = 19;
+
+        System.out.println(s1.name);
+        System.out.println(s1.age);
+
+        sc.close()
+    }
+}
