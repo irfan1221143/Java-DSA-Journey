5bfc22c43b5fdaca0eb2263b7b13f7391008eeec
Irfan <irfanalamnps@gmail.com>
2026-09-04 10:29:23 +0530

    Add Student class with constructor and display method

---
+++ 02_OOP/04_Constructor.java
@@ -0,0 +1,31 @@
+class Student
+{
+    String name;
+    int age;
+    double marks;
+
+    Student(String name, int age, double marks)
+    {
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+    }
+    void display()
+    {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+    }
+}
+
+public class practice
+{
+    public static void main(String args[])
+    {
+        Student st1 = new Student("Irfan", 20, 92.5);
+        Student st2 = new Student("Rahul", 21, 85.5);
+
+        st1.display();
+        st2.display();
+    }
+}
